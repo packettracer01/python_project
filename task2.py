4e9dabe50655612daa9aedd636ae497698105ea4
Ntichika Humphrey Chisom <102184022+packettracer01@users.noreply.github.com>
2025-07-05 09:56:47 +0100

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,1 +1,10 @@
+# Get user's first and last name
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
 
+# Reverse the characters in both names
+reversed_first = first_name[::-1]
+reversed_last = last_name[::-1]
+
+# Print reversed names
+print(f"{reversed_first} {reversed_last}")
