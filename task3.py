419db056a3d0922bc37a0220d1571a261ad08ded
Ntichika Humphrey Chisom <102184022+packettracer01@users.noreply.github.com>
2025-07-05 09:59:20 +0100

    Create task3.py
    A Python program to display the first and last colors from the following list.color_list = ["Red","Green","White" ,"Black"]

---
+++ task3.py
@@ -0,0 +1,5 @@
+color_list = ["Red", "Green", "White", "Black"]
+
+# Display first and last color
+print(f"The first color on the list is : {color_list[0]}")
+print(f"The last color on the list is : {color_list[-1]}")
